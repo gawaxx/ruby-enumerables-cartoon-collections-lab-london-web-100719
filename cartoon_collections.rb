cc175889cc1949bf325167f56a6bb45972d0494b
jules blanc <jules.blanc@unccub.com>
2019-10-03 23:16:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(p_calls)
   p_calls.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_calls)
+  
 end
 
 def find_the_cheese# code an argument here
