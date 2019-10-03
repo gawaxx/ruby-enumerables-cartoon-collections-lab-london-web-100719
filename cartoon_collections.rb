8a96b9c47f754a4035538d2f7ec66f11f352631b
jules blanc <jules.blanc@unccub.com>
2019-10-03 23:28:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(array_calls)
   array_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(chee)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
