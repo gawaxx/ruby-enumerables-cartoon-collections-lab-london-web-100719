faaf8e5d639230c8e51c04e3186b493340952c32
jules blanc <jules.blanc@unccub.com>
2019-10-03 22:59:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  split_names = names.join(' ')
 end
 
 def summon_captain_planet# code an argument here
